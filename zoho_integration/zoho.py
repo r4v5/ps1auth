a1c9c94f2a4b94be1b2c1f2da25bb20b016ae902
hef <hef@pbrfrat.com>
2013-06-05 08:01:32 -0500

    able to pull latest entries from zoho

--- zoho_integration/zoho.py
+++ zoho_integration/zoho.py
@@ -16,10 +16,8 @@ class ZohoClient(object):
         url = ("https://crm.zoho.com/crm/private/json/Contacts/getRecords?" 
                 "authtoken={0}&scope=crmapi&lastModifiedTime={1}&fromIndex=1&toIndex=200&"
                 "sortOrderString=asc&sortColumnString=Modified%20Time".format(self.api_key, most_recent))
-        print url
         response = cache.get(url)
         if not response:
-            print("downloading new info")
             result = urllib2.urlopen(url)
             raw_output = result.read()
             response = json.loads(raw_output)
@@ -28,41 +26,44 @@ class ZohoClient(object):
             cache.set(url, response)
         entries = response[u'response'][u'result'][u'Contacts'][u'row']
         sane_contacts = []
+        # HEFTODO I have a sneaking suspician that zoho returns a list iff there is more than one entry
         for entry in entries:
             e = entry[u'FL']
             sane_contact = dict(map(lambda x: (x['val'], x['content']), e))
             sane_contacts.append(sane_contact)
         return sane_contacts
+    
+    def update_database():
+        try:
+            latest_contact = Contact.objects.latest('modified_time')
+            latest = latest_contact.modified_time.astimezone(pytz.utc).strftime('%Y-%m-%d%%20%H:%M:%S')
+        except Contact.DoesNotExist:
+            latest = "1970-01-01%2012:00:00"
+        contacts = self.get_contacts(latest)
+        for contact in contacts:
+            try:
+                c = Contact.objects.get(contact_id=contact[u'CONTACTID'])
+            except Contact.DoesNotExist:
+                c = Contact(contact_id=contact[u'CONTACTID'])
+            c.first_name = contact[u'First Name']
+            c.last_name = contact['Last Name']
+            try:
+                c.email = contact[u'Email']
+            except KeyError:
+                pprint(contact)
+            try:
+                c.membership_status = contact[u'Membership Status']
+            except KeyError:
+                pprint(contact)
+            modified_time = datetime.strptime(contact[u'Modified Time'], '%Y-%m-%d %H:%M:%S')
+            modified_time = modified_time.replace(tzinfo=pytz.utc)
+            pprint(modified_time)
+            c.modified_time = modified_time
+            c.save()
+
 
 if __name__ == '__main__':
     from pprint import pprint
-    try:
-        latest_contact = Contact.objects.latest('modified_time')
-        latest = latest_contact.modified_time.astimezone(pytz.utc).strftime('%Y-%m-%d%%20%H:%M:%S')
-    except Contact.DoesNotExist:
-        latest = "1970-01-01%2012:00:00"
     zc = ZohoClient()
-    contacts = zc.get_contacts(latest)
-    for contact in contacts:
-        try:
-            c = Contact.objects.get(contact_id=contact[u'CONTACTID'])
-        except Contact.DoesNotExist:
-            c = Contact(contact_id=contact[u'CONTACTID'])
-        c.first_name = contact[u'First Name']
-        c.last_name = contact['Last Name']
-        try:
-            c.email = contact[u'Email']
-        except KeyError:
-            pprint(contact)
-        try:
-            c.membership_status = contact[u'Membership Status']
-        except KeyError:
-            pprint(contact)
-        modified_time = datetime.strptime(contact[u'Modified Time'], '%Y-%m-%d %H:%M:%S')
-        modified_time = modified_time.replace(tzinfo=pytz.utc)
-        pprint(modified_time)
-        c.modified_time = modified_time
-        c.save()
-
-    #pprint(zc.get_contacts())
+    zc.update_database()
 
