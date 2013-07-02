9ec925427a021794dd5e25dabd7661764df0e34e
hef <hef@pbrfrat.com>
2013-07-02 14:04:41 -0500

    fixed a typo

--- settings/base.py
+++ settings/base.py
@@ -154,8 +154,8 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-            'include_html': True
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
         }
     },
     'loggers': {
