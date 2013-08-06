1965e4c2960cf40c9e30d33f666eec4135cf6318
hef <hef@pbrfrat.com>
2013-08-06 18:30:12 -0500

    fabfile can now deploy to production and staging

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,23 @@
 from fabric.api import *
 from cuisine import *
 
-env.hosts = ['arbitrarion.com']
+def staging():
+    env.root = '/srv/http/arbitrarion.com/app/'
+    env.hosts = ['arbitrarion.com']
+    env.mode = 'staging'
+
+def production():
+    env.root = '/srv/http/members.pumpingstationone.org/app'
+    env.hosts = ['vm.pumpingstationone.org']
+    env.mode = 'production'
 
 def deploy():
-    with cd('/srv/http/arbitrarion.com/app/'):
+    with cd('%(root)s' % env):
         run('git pull')
         with prefix('workon ps1auth'):
-            run('pip install -r requirements/staging.txt')
+            run('pip install -r requirements/%(mode)s.txt' % env)
             run('./manage.py syncdb --noinput')
             run('./manage.py migrate --noinput')
             run('./manage.py collectstatic --noinput')
-            run('killall /home/hef/.virtualenvs/ps1auth/bin/python2')
+            run('killall /home/hef/.virtualenvs/ps1auth/bin/python2', warn_only=True)
 
