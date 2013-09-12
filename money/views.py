24f5ed6457cdab1425fbc7f6b1953550487f7139
hef <hef@pbrfrat.com>
2013-09-12 08:57:41 -0500

    found a really lazy way to examine data
    Just going to raise exception

--- money/views.py
+++ money/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.dispatch import reciever
+from billing.signals import transaction_was_successful
+
+
+@reciever(transaction_was_successful)
+def handle_successful_transaction(sender, type, response):
+    raise Exception
+
+@reiver(transaction_was_unsuccessful)
+def handle_unsuccessful_transaction(sender, type, response):
+    raise Exception
