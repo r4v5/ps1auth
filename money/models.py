6350044165e8e3ca0a0c66df4ce78d25a92087ab
hef <hef@pbrfrat.com>
2013-09-12 21:03:42 -0500

    accepting other args

--- money/models.py
+++ money/models.py
@@ -3,11 +3,11 @@ from django.dispatch import receiver
 from billing.signals import transaction_was_successful, transaction_was_unsuccessful
 
 @receiver(transaction_was_successful)
-def handle_successful_transaction(sender, type, response):
+def handle_successful_transaction(sender, type, response, **kwargs):
     raise Exception
 
 @receiver(transaction_was_unsuccessful)
-def handle_unsuccessful_transaction(sender, type, response):
+def handle_unsuccessful_transaction(sender, type, response, **kwargs):
     raise Exception
 
 #transaction_was_successful.connect(handle_successful_transaction)
