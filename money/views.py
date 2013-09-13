42f4e7ecac028737f2cd7dac62936364e77221f5
hef <hef@pbrfrat.com>
2013-09-12 20:48:06 -0500

    moving signal handlers to model

--- money/views.py
+++ money/views.py
@@ -1,16 +1,1 @@
 # Create your views here.
-from django.dispatch import reciever
-from billing.signals import transaction_was_successful, transaction_was_unsuccessful
-
-
-#@reciever(transaction_was_successful)
-def handle_successful_transaction(sender, type, response):
-    raise Exception
-
-#@reiver(transaction_was_unsuccessful)
-def handle_unsuccessful_transaction(sender, type, response):
-    raise Exception
-
-transaction_was_successful.connect(handle_successful_transaction)
-transaction_was_unsuccessful.connect(handle_unsuccessful_transaction)
-
