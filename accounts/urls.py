7259561c4d671c046c48f6471c45fc0df9dabcec
hef <hef@pbrfrat.com>
2013-11-17 20:05:31 -0600

    fixing password reset bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import patterns, include, url
 import accounts.views
 import accounts.forms
-
+#import django.contrib.auth.views.password_reset_done
 
 urlpatterns = patterns('',
     url(r'login/$', 'django.contrib.auth.views.login', {}),
     url(r'logout/$', 'django.contrib.auth.views.logout', {}),
     url(r'password_change/$', 'django.contrib.auth.views.password_change', {}),
     url(r'password_change_done/$', 'django.contrib.auth.views.password_change_done', {}),
-    url(r'password_reset/$', 'django.contrib.auth.views.password_reset', {'password_reset_form': accounts.forms.PasswordResetForm}),
+    url(r'password_reset/$', 'django.contrib.auth.views.password_reset',
+        {
+            'password_reset_form': accounts.forms.PasswordResetForm,
+            'post_reset_redirect': 'django.contrib.auth.views.password_reset_done',
+        }),
     url(r'password_reset_done/$', 'django.contrib.auth.views.password_reset_done', {}),
     #url(r'password_reset_confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)$', 'django.contrib.auth.views.password_reset_confirm', {}),
     #url(r'password_reset_confirm/(?P<uidb36>[0-9A-Za-z\-]+)/(?P<token>[0-9A-Za-z\-]+)$', 'django.contrib.auth.views.password_reset_confirm', {}),
