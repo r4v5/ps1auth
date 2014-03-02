63dcb05b691edde154bf346c215df20e7f531e94
hef <hef@pbrfrat.com>
2014-03-01 18:47:19 -0600

    added rfid ad group support

--- rfid/admin.py
+++ rfid/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Resource, RFIDNumber
+from .models import AdGroupResource, Resource, RFIDNumber
 
 admin.site.register(Resource)
 admin.site.register(RFIDNumber)
+admin.site.register(AdGroupResource)
--- rfid/models.py
+++ rfid/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 from django.conf import settings
 
 
@@ -12,14 +12,25 @@ class Resource(models.Model):
         """
 
         try:
-            RFIDNumber.objects.get(pk=tag.pk)
-            return True
+            rfid = RFIDNumber.objects.get(pk=tag.pk)
+            return rfid.user.is_active()
         except RFIDNumber.DoesNotExist:
             return False
 
     def __unicode__(self):
         return self.name
 
+class AdGroupResource(Resource):
+    """
+    Resource that matches against AD groups.
+    """
+    ad_group = models.CharField(max_length=255)
+
+    def is_allowed(self, tag):
+        try:
+            return tag.user.is_active() and self.ad_group in self.ldap_user['memberOf']
+        except KeyError:
+            return False
 
 class RFIDNumber(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
