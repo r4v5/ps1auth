c15a6961147207579497d4c3ab3ec758a029d77a
Justin T. Conroy <Justin@Conroy.in>
2014-06-24 00:44:29 -0500

    Improves memberpoint admin interface.

--- memberpoint/admin.py
+++ memberpoint/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import PointAccount
+from .models import PointAccount, PointTransaction
 
-admin.site.register(PointAccount)
+class TransactionInline(admin.TabularInline):
+    model = PointTransaction
+    extra = 0
+
+class PointAdmin(admin.ModelAdmin):
+    fields = ['user']
+    inlines = [TransactionInline]
+    #search_fields = ['user']
+
+admin.site.register(PointAccount, PointAdmin)
+
