7e1447c1317fc77f47652ab0a4670b21f18a2b8e
hef <hef@pbrfrat.com>
2015-02-19 23:36:01 -0600

    moving signup views over

--- ps1auth/urls.py
+++ ps1auth/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse
 from django.views.generic import RedirectView
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import debug_toolbar
 
 admin.autodiscover()
 
@@ -14,16 +12,20 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'auth.views.home', name='home'),
     url(r'^$', RedirectView.as_view(permanent=False, url='/mm/member_list')),
-    url(r'^zinc/member_list$', RedirectView.as_view(permanent=False, url='/mm/member_list')),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^rfid/', include('rfid.urls')),
-    url(r'^zinc/', include('zoho_integration.urls')),
-    #url(r'^pp/', include('paypal_integration.urls')),
-    url(r'^mm/', include('member_management.urls')),
-    url(r'^memberpoints/', include('memberpoint.urls')),
-    url(r'^ckeditor/', include('ckeditor.urls')),
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^ckeditor/', include('ckeditor.urls')),
+    url(r'^memberpoints/', include('memberpoint.urls')),
+    url(r'^mm/', include('member_management.urls')),
+    #url(r'^pp/', include('paypal_integration.urls')),
+    url(r'^rfid/', include('rfid.urls')),
+    url(r'^signup/', include('signup.urls')),
+)
+
+# Cool URIs don't change
+urlpatterns += patterns('',
+    url(r'^zinc/member_list$', RedirectView.as_view(permanent=False, url='/mm/member_list')),
+    url(r'^zinc/activate/$', RedirectView.as_view(permanent=False, url='/signup/activate')),
 )
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
